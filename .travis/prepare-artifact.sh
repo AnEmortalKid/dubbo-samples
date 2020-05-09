4695a2947f93a107ffc74da39053d692f694477e
hengyunabc <hengyunabc@gmail.com>
2020-05-09 10:56:15 +0800

    revert_5710

--- .travis/prepare-artifact.sh
+++ .travis/prepare-artifact.sh
@@ -6,7 +6,8 @@ if [[ -z "${RELEASE_BRANCH}" ]]; then
 else
   git clone https://github.com/apache/dubbo.git
   cd dubbo
-  git checkout ${RELEASE_BRANCH}
+  echo "dubbo checkout revert_5710"
+  git checkout revert_5710
   ls
   ./mvnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
   export INTERGARTION_TEST_VERSION=${RELEASE_BRANCH}

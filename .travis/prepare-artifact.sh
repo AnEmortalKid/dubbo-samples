cfd79409eeed69bbcbb55b16ab6f46821d7ff5f9
hengyunabc <hengyunabc@gmail.com>
2020-05-09 11:11:55 +0800

    revert_5710

--- .travis/prepare-artifact.sh
+++ .travis/prepare-artifact.sh
@@ -1,18 +1,26 @@
 #! /usr/bin/env bash
 env
 
-if [[ -z "${RELEASE_BRANCH}" ]]; then
-  echo "RELEASE_BRANCH: ${RELEASE_BRANCH}"
-  export INTERGARTION_TEST_VERSION=2.7.7-SNAPSHOT
-else
-  git clone https://github.com/apache/dubbo.git
-  cd dubbo
-  echo "dubbo checkout revert_5710"
-  git checkout revert_5710
-  ls
-  ./mvnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
-  export INTERGARTION_TEST_VERSION=${RELEASE_BRANCH}
-  cd ..
-fi
+
+git clone https://github.com/apache/dubbo.git
+cd dubbo
+echo "dubbo checkout revert_5710"
+git checkout revert_5710
+ls
+./mvnw clean install -DskipTests -Drevision=2.7.7-SNAPSHOT
+
+# if [[ -z "${RELEASE_BRANCH}" ]]; then
+#   echo "RELEASE_BRANCH: ${RELEASE_BRANCH}"
+#   export INTERGARTION_TEST_VERSION=2.7.7-SNAPSHOT
+# else
+#   git clone https://github.com/apache/dubbo.git
+#   cd dubbo
+#   echo "dubbo checkout revert_5710"
+#   git checkout revert_5710
+#   ls
+#   ./mvnw clean install -DskipTests -Drevision=${RELEASE_BRANCH}
+#   export INTERGARTION_TEST_VERSION=${RELEASE_BRANCH}
+#   cd ..
+# fi
 
 env|grep INTERGARTION_TEST_VERSION
